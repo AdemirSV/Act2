5b97c41dad05c35e5687608dcdd3521a3bf23d90
Ademir Stefano Santos Vasquez <I202319028@cibertec.edu.pe>
2025-10-14 07:53:12 -0500

    Creacion

---
+++ src/main/java/com/Act2/Act2/Act2Application.java
@@ -0,0 +1,13 @@
+package com.Act2.Act2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Act2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Act2Application.class, args);
+	}
+
+}
